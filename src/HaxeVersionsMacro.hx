ea54109bf1e518f73c645001705b90a719f1dc93
AlexHaxe <Alexander.Blum@gmail.com>
2020-03-09 18:17:21 +0100

    cleanup macro code

--- src/HaxeVersionsMacro.hx
+++ src/HaxeVersionsMacro.hx
@@ -45,41 +45,6 @@ class HaxeVersionsMacro {
 	#end
 
 	macro public static function macroMapOutput2Target():Expr {
-		var output = Compiler.getOutput();
-
-		if (~/\.n$/.match(output)) {
-			return macro "Neko";
-		}
-		if (~/\.es6\.js$/.match(output)) {
-			return macro "NodeJS (ES6)";
-		}
-		if (~/\.js$/.match(output)) {
-			return macro "NodeJS";
-		}
-		if (~/\.hl$/.match(output)) {
-			return macro "Hashlink";
-		}
-		if (~/\.c$/.match(output)) {
-			return macro "Hashlink/C";
-		}
-		if (~/cpp$/.match(output)) {
-			return macro "C++";
-		}
-		if (~/cs$/.match(output)) {
-			return macro "C#";
-		}
-		if (~/java$/.match(output)) {
-			return macro "Java";
-		}
-		if (~/jvm$/.match(output)) {
-			return macro "JVM";
-		}
-		if (~/php$/.match(output)) {
-			return macro "PHP";
-		}
-		if (~/\.py$/.match(output)) {
-			return macro "Python";
-		}
-		return macro "eval";
+		return macro $v{mapOutput2Target()};
 	}
 }
